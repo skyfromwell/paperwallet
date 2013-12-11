9dfeda0078b8479336da266477af58c34e24dacc
Sha Zhou <sha.zhou@gmail.com>
2013-12-11 10:42:00 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+cmd = {'bitcoin':["./vanitygen", "-q", "-t", "1", "-s", "/dev/random", None],
+       'litecoin':["/vanitygen-litecoin", "-q", "-L", "-t", "1", "-s", "/dev/randon", None],
+       }
+home_path = '/Users/pi/wallet/'
+image_name = {'bitcoin':'btc-wallet-blank.bmp',
+              'bitcoin-enc':'btc-wallet-enc.bmp',
+              'litecoin':'ltc-wallet-blank.bmp',
+              'litecoin-enc':'ltc-wallet-enc.bmp'}
